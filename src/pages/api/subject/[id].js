3074aac4e4b219c6fd73d2daef79d053ad56a3b0
Vũ Trung Nghĩa <vutrungnghiahn@gmail.com>
2023-04-20 19:15:39 +0700

    fix: api

--- src/pages/api/subject/[id].js
+++ src/pages/api/subject/[id].js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
     return;
   }
   if (req.method === "PUT") {
-    const data = await updatePostApi(id, req.body);
+    const data = await updateSubjectApi(id, req.body);
     res.status(200).json(data);
     return;
   }
