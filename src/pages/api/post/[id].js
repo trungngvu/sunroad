d5fb64f08d32fefa8c650223206efbb066d12b8d
Vũ Trung Nghĩa <vutrungnghiahn@gmail.com>
2023-04-20 19:09:25 +0700

    fix: api update

--- src/pages/api/post/[id].js
+++ src/pages/api/post/[id].js
@@ -20,14 +20,13 @@ export const updatePostApi = async (id, updates) =>
   });
 
 export default async function handler(req, res) {
+  const { id } = req.query;
   if (req.method === "GET") {
-    const { id } = req.query;
     const data = await postApi(id);
     res.status(200).json(data);
     return;
   }
   if (req.method === "PUT") {
-    const { id } = req.query;
     const data = await updatePostApi(id, req.body);
     res.status(200).json(data);
     return;
--- src/pages/api/subject/[id].js
+++ src/pages/api/subject/[id].js
@@ -10,10 +10,21 @@ export const subjectApi = async (id) =>
     },
   });
 
+export const updateSubjectApi = async (id, updates) =>
+  await prisma.subject.update({
+    where: { id: parseInt(id) },
+    data: updates,
+  });
+
 export default async function handler(req, res) {
+  const { id } = req.query;
   if (req.method === "GET") {
-    const { id } = req.query;
     const data = subjectApi(id);
+    res.status(200).json(data);
+    return;
+  }
+  if (req.method === "PUT") {
+    const data = await updatePostApi(id, req.body);
     res.status(200).json(data);
     return;
   }
