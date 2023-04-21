b7d9c31a5426e18cb181770a18494359ca185a95
Vũ Trung Nghĩa <vutrungnghiahn@gmail.com>
2023-04-21 21:41:48 +0700

    fix: cors '

--- next.config.js
+++ next.config.js
@@ -1,25 +1,9 @@
 /** @type {import('next').NextConfig} */
-const webpack = require("webpack");
 const nextConfig = {
   reactStrictMode: true,
 };
+module.exports = nextConfig;
 
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery",
-        "window.jQuery": "jquery",
-      })
-    );
-
-    // Important: return the modified config
-    return config;
-  },
-};
-
-// next.config.js
 module.exports = {
   async headers() {
     return [
@@ -43,5 +27,3 @@ module.exports = {
     ];
   },
 };
-
-module.exports = nextConfig;
